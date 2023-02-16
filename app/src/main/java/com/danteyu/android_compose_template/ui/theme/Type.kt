210571dfe69effc44fe28a239bfe27a0693ed0a6
George <aptx113@gmail.com>
2023-02-16 11:35:43 +0800

    add libs dependencies

--- app/src/main/java/com/danteyu/android_compose_template/ui/theme/Type.kt
+++ app/src/main/java/com/danteyu/android_compose_template/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.danteyu.android_template_compose.ui.theme
+package com.danteyu.android_compose_template.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
