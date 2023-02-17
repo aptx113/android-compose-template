c2302a6229a4080ded3b1443d0c3ec3cb4df37ce
George <aptx113@gmail.com>
2023-02-17 10:49:21 +0800

    add project extensions for compose, general libs, jacoco and apk test

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -5,16 +5,16 @@ plugins {
 group = "com.danteyu.android_template_compose.buildlogic"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 dependencies {
-//    compileOnly(libs.android.gradlePlugin)
-//    compileOnly(libs.kotlin.gradlePlugin)
-//    compileOnly(libs.firebase.performance.gradle)
-//    compileOnly(libs.firebase.crashlytics.gradle)
-//    compileOnly(libs.ksp.gradlePlugin)
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.firebase.performance.gradle)
+    compileOnly(libs.firebase.crashlytics.gradle)
+    compileOnly(libs.ksp.gradlePlugin)
 }
 
 gradlePlugin {
--- build-logic/convention/src/main/kotlin/AndroidApplicationComposeConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationComposeConventionPlugin.kt
@@ -1,12 +1,14 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.getByType
+import com.android.build.api.dsl.ApplicationExtension
 
 class AndroidApplicationComposeConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        with(target){
+        with(target) {
             pluginManager.apply("com.android.application")
-//            val extension = extensions.getByType<ApplicationExtension>()
+            val extension = extensions.getByType<ApplicationExtension>()
+
         }
     }
 }
