007bfb9626dbec39ea9d9c1cea245151ae628e3f
George <aptx113@gmail.com>
2023-02-20 16:02:31 +0800

    configure dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 import com.danteyu.android_compose_template.AppBuildType
-import com.danteyu.android_compose_template.config.DefaultConfig
+import com.danteyu.android_compose_template.config.DefaultConfigs
 
 plugins {
     id("app.android.application")
@@ -15,11 +15,11 @@ val keystorePropertiesFile = rootProject.file("keystore.properties")
 
 android {
     defaultConfig {
-        applicationId = DefaultConfig.APPLICATION_ID
-        versionCode = DefaultConfig.VERSION_CODE
-        versionName = DefaultConfig.VERSION_NAME
+        applicationId = DefaultConfigs.APPLICATION_ID
+        versionCode = DefaultConfigs.VERSION_CODE
+        versionName = DefaultConfigs.VERSION_NAME
 
-        testInstrumentationRunner = DefaultConfig.TEST_INSTRUMENTATION_RUNNER
+        testInstrumentationRunner = DefaultConfigs.TEST_INSTRUMENTATION_RUNNER
         vectorDrawables {
             useSupportLibrary = true
         }
@@ -63,68 +63,28 @@ android {
     testOptions {
         unitTests { isIncludeAndroidResources = true }
     }
-    namespace = DefaultConfig.NAME_SPACE
+    namespace = DefaultConfigs.NAME_SPACE
 }
 
 dependencies {
 
-}
+    androidTestImplementation(libs.androidx.navigation.testing)
+    androidTestImplementation(libs.accompanist.testharness)
+    debugImplementation(libs.androidx.compose.ui.testManifest)
 
-//android {
-//    namespace 'com.danteyu.android_template_compose'
-//    compileSdk 33
-//
-//    defaultConfig {
-//        applicationId "com.danteyu.android_template_compose"
-//        minSdk 28
-//        targetSdk 33
-//        versionCode 1
-//        versionName "1.0"
-//
-//        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
-//        vectorDrawables {
-//            useSupportLibrary true
-//        }
-//    }
-//
-//    buildTypes {
-//        release {
-//            minifyEnabled false
-//            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
-//        }
-//    }
-//    compileOptions {
-//        sourceCompatibility JavaVersion.VERSION_1_8
-//        targetCompatibility JavaVersion.VERSION_1_8
-//    }
-//    kotlinOptions {
-//        jvmTarget = '1.8'
-//    }
-//    buildFeatures {
-//        compose true
-//    }
-//    composeOptions {
-//        kotlinCompilerExtensionVersion '1.2.0'
-//    }
-//    packagingOptions {
-//        resources {
-//            excludes += '/META-INF/{AL2.0,LGPL2.1}'
-//        }
-//    }
-//}
-//
-//dependencies {
-//
-//    implementation 'androidx.core:core-ktx:1.7.0'
-//    implementation 'androidx.lifecycle:lifecycle-runtime-ktx:2.3.1'
-//    implementation 'androidx.activity:activity-compose:1.3.1'
-//    implementation "androidx.compose.ui:ui:$compose_version"
-//    implementation "androidx.compose.ui:ui-tooling-preview:$compose_version"
-//    implementation 'androidx.compose.material3:material3:1.0.0-alpha11'
-//    testImplementation 'junit:junit:4.13.2'
-//    androidTestImplementation 'androidx.test.ext:junit:1.1.5'
-//    androidTestImplementation 'androidx.test.espresso:espresso-core:3.5.1'
-//    androidTestImplementation "androidx.compose.ui:ui-test-junit4:$compose_version"
-//    debugImplementation "androidx.compose.ui:ui-tooling:$compose_version"
-//    debugImplementation "androidx.compose.ui:ui-test-manifest:$compose_version"
-//}
+    implementation(libs.accompanist.systemuicontroller)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.compose.runtime)
+    implementation(libs.androidx.lifecycle.runtimeCompose)
+    implementation(libs.androidx.compose.runtime.tracing)
+    implementation(libs.androidx.compose.material3.windowSizeClass)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.window.manager)
+    implementation(libs.androidx.profileinstaller)
+
+    implementation(libs.coil.kt)
+}
--- build-logic/convention/src/main/kotlin/com/danteyu/android_compose_template/config/DefaultConfigs.kt
+++ build-logic/convention/src/main/kotlin/com/danteyu/android_compose_template/config/DefaultConfigs.kt
@@ -1,6 +1,6 @@
 package com.danteyu.android_compose_template.config
 
-object DefaultConfig {
+object DefaultConfigs {
     const val APPLICATION_ID = "com.danteyu.android_compose_template"
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0.0"
